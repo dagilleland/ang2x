cb8649b44fd8391c7cd5b1601025735a6438fe9d
dagilleland <dagilleland@shaw.ca>
2016-04-07 03:06:17 -0600

    Working version of component loader

--- _App/app.ts
+++ _App/app.ts
@@ -1,9 +1,36 @@
-import {Component} from 'angular2/core';
+import {Component, DynamicComponentLoader, ElementRef} from 'angular2/core';
+
+function compileToComponent(template:string, directives:any[]) {
+  @Component({ 
+    selector: 'fake', 
+    template , directives
+  })
+  class FakeComponent {};
+  return FakeComponent;
+}
+
+
+@Component({
+  selector: 'hello',
+  template: '<h1>Hello, Angular!</h1>'
+})
+class Hello {}
 
 @Component({
     selector: 'core-app',
-    templateUrl: 'components/app.html'
+    templateUrl: 'components/app.html',
+    directives: [Hello]
 })
 export class App {
-    
+    constructor(protected loader: DynamicComponentLoader, protected elementRef: ElementRef) {
+  }
+  ngOnInit() {
+    const someDynamicHtml = `<hello></hello><h2>${Date.now()}</h2>`;
+
+    this.loader.loadIntoLocation(
+      compileToComponent(someDynamicHtml, [Hello]),
+      this.elementRef,
+      'container'
+    );
+  }
 }
